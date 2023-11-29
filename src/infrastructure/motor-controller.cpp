4672a1aaf6a2d958ada82d3c59b10ba35772bb37
lstop10 <lsjtop10@gmail.com>
2023-11-29 20:16:55 +0900

    Add: add motor contorol code to motor-controller

--- src/infrastructure/motor-controller.cpp
+++ src/infrastructure/motor-controller.cpp
@@ -1,42 +1,42 @@
 #include <Arduino.h>
+#include <TimerOne.h>
+#include <MsTimer2.h>
 #include "motor-controller.h"
 #include "pins.h"
 
-MotorController::MotorController(int min, int max)
+MotorController::MotorController(int pwmPin, int breakPin)
 {
 
-    pwmPin = MOTOR_PWM_PIN;
-    brakePin = MOTOR_BREAK_PIN;
+    pwmPin = pwmPin;
+    brakePin = breakPin;
+    slope = 8;
 
-    minVal = min;
-    maxVal = max;
+    limit = 50;
 
     pinMode(pwmPin, OUTPUT);
     pinMode(brakePin, OUTPUT);
 
-    /*
+    //Brake
+    pinMode(brakePin, OUTPUT); //Brake, Output
+    digitalWrite(brakePin, LOW); //Brake, '0'
+
     //PWM
     Timer1.initialize(); //Timer1
     Timer1.pwm(pwmPin, 0); //PWM, Duty 0%
     Timer1.setPeriod(50); //PWM period 20kHz
     Timer1.setPwmDuty(pwmPin, 0); //PWM, Duty 0%
 
-    //Direction
-    pinMode(Direction, OUTPUT); //Direction, Output
-    digitalWrite(Direction, LOW); //Direction, '0'
-
-    //Brake
-    pinMode(Brake, OUTPUT); //Brake, Output
-    digitalWrite(Brake, LOW); //Brake, '0'
-
-    //Timer interrupt
-    MsTimer2::set(10/Slope, flash); //Timer interrupt period
-    MsTimer2::start(); //Timer interrupt start
-    */
 }
 
 MotorController::~MotorController()
 {
+
+}
+
+void MotorController::flash()
+{
+    if(duty < dutyRef) duty++;
+    Timer1.setPwmDuty(pwmPin, duty); //PWM, Duty
 }
 
 void MotorController::On(int power)
@@ -45,13 +45,25 @@ void MotorController::On(int power)
     {
         return;
     }
+
+    if(power > limit)
+    {
+        power = limit;
+    }
+
+    duty = map(power, 0, 100, 0, 1024);
     
-    digitalWrite(brakePin, HIGH);
-    digitalWrite(pwmPin, map(power, 0, 100, minVal, maxVal));
+    digitalWrite(brakePin, LOW);
+
+    //Timer interrupt
+    MsTimer2::set(10/slope, (void(*)())&flash); //Timer interrupt period
+    MsTimer2::start(); //Timer interrupt start
+
 }
 
 void MotorController::Off()
 {
-    digitalWrite(brakePin, LOW);
-    digitalWrite(pwmPin, LOW);
+    digitalWrite(brakePin, HIGH);
+
+    MsTimer2::stop();
 }
--- src/infrastructure/motor-controller.h
+++ src/infrastructure/motor-controller.h
@@ -3,19 +3,25 @@
 class MotorController
 {
 private:
+
     int pwmPin;
     int brakePin;
 
-    int minVal;
-    int maxVal;
+    /// @brief 모터 제한 출력의 퍼센테이지 단위
+    int limit;
 
+    int slope;
+    int duty;
+    int dutyRef;
+
+    void flash();
 public:
    
-    /// @brief 
+    /// @brief turn on the motor
     /// @param power 퍼센트 단위의 모터의 회전 속도
     void On(int power);
     void Off();
-    MotorController::MotorController(int min, int max);
+    MotorController::MotorController(int pwmPin, int breakPin);
     MotorController::~MotorController();
 };
 
