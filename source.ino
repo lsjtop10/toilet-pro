a66b940ce25b6ebd7acf95260e690614d62afd12
lsjtop10 <lsjtop10@gmail.com>
2023-12-13 21:03:31 +0900

    Fix: Fix source.ino
    Fix paramator constructor of MotorController()
    Fix motor test logic
    Fix led test logic
    Fix delay time to check the toilet is broken
    Add logic to display green signal

--- source.ino
+++ source.ino
@@ -2,22 +2,23 @@
 #include "src/infrastructure/level-sensor-mgr.h"
 #include "src/infrastructure/led-mgr.h"
 #include "src/infrastructure/motor-controller.h"
+#include "src/debug.h"
 
 #define LED_R_PIN 3
 #define LED_Y_PIN 4
 #define LED_G_PIN 5
-#define MOTOR_PWM_PIN 9
-#define MOTOR_BREAK_PIN 10 
+#define MOTOR_PWM_PIN 10
+#define MOTOR_BREAK_PIN 9 
 #define LEVEL_IN_PIN 6
 #define TRIG_PIN 7
 #define ECHO_PIN 8
 
-LEDMgr led = LEDMgr(LED_R_PIN, LED_Y_PIN,LED_G_PIN);
+LEDMgr led = LEDMgr(LED_R_PIN, LED_Y_PIN, LED_G_PIN);
 LevelSensorMgr levelSensor = LevelSensorMgr(LEVEL_IN_PIN);
 CoverSensorMgr coverSensor = CoverSensorMgr(TRIG_PIN, ECHO_PIN);
-MotorController motor = MotorController(0,255);
+MotorController motor = MotorController(MOTOR_PWM_PIN, MOTOR_BREAK_PIN);
 
-const int MotorEnableTime = 10*1000;
+const int MotorEnableTime = (int)2.5*1000;
 
 void testLevelSensor();
 void testCoverSensor();
@@ -25,18 +26,24 @@ void testLEDs();
 
 void setup()
 {
-    Serial.begin(9600);
+    debug(Serial.begin(9600));
     pinMode(13,OUTPUT);
+
+    digitalWrite(13, LOW);
 }
 
+// test logics
 void testMotor()
 {
-   motor.Off();
+    //motor.Off();
 
-   motor.On(25);
-   delay(5000);
+    digitalWrite(13, HIGH);
+    motor.On(100);
+    delay(5000);
 
-   motor.Off();
+    digitalWrite(13, LOW);
+    motor.Off();
+    delay(2000);
 }
 
 void testLevelSensor()
@@ -57,8 +64,8 @@ void testCoverSensor()
     if(coverSensor.IsOpen())
     {
         digitalWrite(13,HIGH);
-        delay(500);
     }
+    delay(1000);
 }
 
 void testLEDs()
@@ -68,7 +75,7 @@ void testLEDs()
     delay(1000);
     led.Write(false,true,false);
     delay(1000);
-    led.Write(true,false,true);
+    led.Write(false,false,true);
     delay(1000);
 }
 
@@ -77,9 +84,10 @@ void testLEDs()
 void loop()
 {   
 
+#ifndef DEBUG
     if(!coverSensor.IsOpen() && levelSensor.IsFull())
     {
-        delay(5*1000);
+        delay(2500);
 
         if(!levelSensor.IsFull())
         {
@@ -97,4 +105,12 @@ void loop()
     {
         led.Write(false,true,false);
     }
+    else if(coverSensor.IsOpen() && !levelSensor.IsFull())
+    {
+        led.Write(false,false,true);
+    }
+#else
+//테스트 로직 기술하는 부분
+testMotor();
+#endif
 }
